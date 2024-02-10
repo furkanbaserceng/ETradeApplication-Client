7896a126d647538e2be1b3cbdb706fab512a616f
Furkan BAŞER <furkanbaserceng@outlook.com>
2024-02-11 00:12:40 +0300

    Uygulamaya, Bootstrap 5.3.2 kütüphanesi altyapısı kazandırıldı. Jquery desteği de getirildi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ETradeClient';
+
+  ngOnInit(){
+
+    $(document).ready(function(){
+
+        alert("hello");
+
+    })
+
+  }
+
 }
