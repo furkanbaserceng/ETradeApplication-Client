5e4864c2510982eae2c74d69c5735f29f27663fa
Furkan BAŞER <furkanbaserceng@outlook.com>
2024-02-10 17:39:04 +0300

    Uygulama ana rotası oluşturuldu. Lazy loading ile talep halinde yüklenecek sayfalar ile direkt yüklenmesi gereken sayfalar uygulamanın şimdiki ihtiyacına binaen oluşturuldu.İleride durumlara göre rotalara eager loading yada lazy loading kazandırılabilecek yapı sistemde mevcuttur.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './ui/components/home/home.component';
+import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.component';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"products",loadChildren:()=>import("./ui/components/product/product.module").then(m=>m.ProductModule)},
+  {path:"basket",loadChildren:()=>import("./ui/components/basket/basket.module").then(m=>m.BasketModule)},
+  {path:"admin",component:AdminLayoutComponent,children:[
+    
+    {path:"",component:DashboardComponent},
+    {path:"customers",loadChildren:()=>import("./admin/components/customer/customer.module").then(m=>m.CustomerModule)},
+    {path:"orders",loadChildren:()=>import("./admin/components/order/order.module").then(m=>m.OrderModule)},
+    {path:"products",loadChildren:()=>import("./admin/components/product/product.module").then(m=>m.ProductModule)}
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
